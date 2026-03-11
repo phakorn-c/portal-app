71d8399b76fffda65328bd1d3fbf440f83b87fc7
phakorn-c <phakorn.ch@kkumail.com>
2026-03-11 21:08:36 +0700

    Replace default Laravel welcome page with procurement search page and remove Laravel-specific UI elements

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,7 @@ use Inertia\Inertia;
 use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return Inertia::render('welcome', [
-        'canRegister' => Features::enabled(Features::registration()),
-    ]);
+    return Inertia::render('procurement/search');
 })->name('home');
 
 Route::get('dashboard', function () {
